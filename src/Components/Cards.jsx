cc917ff2b0f12be9d515f389ee4fbb521a2f7c38
SHERLOCKx90 <sherlockx90@gmail.com>
2025-02-09 22:16:02 +0530

    fixed->cards-added->content

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -4,11 +4,14 @@ import FeatureCard from './FeatureCard'
 const Cards = () => {
     return (
         <div className="carousel carousel-vertical rounded-box h-96 mb-10 cursor-pointer transition-all">
-            <div className="carousel-item h-full">
-                <FeatureCard imgsrc={"/image-1.jpg"}/>
+            <div className="carousel-item h-full sm:max-w-full">
+                <FeatureCard imgsrc={"/image-1.jpg"} headline="💡 Ask Questions, Get Answers Instantly" details="Leverage our advanced conversational AI to interact with your data naturally. No more writing complex queries—simply ask, and QueryLensAI transforms your intent into actionable insights." />
             </div>
-            <div className="carousel-item h-full">
-                <FeatureCard imgsrc={"/image-2.jpg"}/>
+            <div className="carousel-item h-full sm:max-w-full">
+                <FeatureCard imgsrc={"/image-5.jpg"} headline="⚙️ Stay Ahead with Real-Time Analytics" details="Monitor trends and track performance instantly with real-time data analysis. QueryLensAI empowers you with growth metrics like week-over-week and month-over-month comparisons to make timely, informed decisions." />
+            </div>
+            <div className="carousel-item h-full sm:max-w-full">
+                <FeatureCard imgsrc={"/image-2.jpg"} headline="✅ Your Data, Any Source, Unified Insights" details="Seamlessly connect to CSVs, relational databases, or live data streams. QueryLensAI adapts to your data ecosystem, providing a unified platform to analyze diverse datasets effortlessly." />
             </div>
         </div>
     )
--- src/Components/FeatureCard.jsx
+++ src/Components/FeatureCard.jsx
@@ -1,14 +1,14 @@
 import { MoveRight } from 'lucide-react';
 
-const FeatureCard = ({imgsrc}) => {
+const FeatureCard = ({imgsrc, headline, details}) => {
     return (
-        <div className='flex flex-col sm:flex-row sm:items-start justify-center items-center bg-white rounded-xl'>
+        <div className='flex flex-col sm:flex-row sm:items-start justify-center items-center bg-white rounded-xl sm:gap-5'>
             <div className='w-full sm:w-1/2 sm:h-full h-[50%] object-fill rounded-xl overflow-hidden'>
                 <img src={imgsrc} alt="image#1" className='w-full h-full object-cover' />
             </div>
             <div className='flex flex-col justify-start items-start p-5 sm:w-1/2'>
-                <h1 className='text-3xl font-semibold text-black'>Get Data Insights Effortlessly.</h1>
-                <p className='font-medium text-gray-500'>Access data insights using via query search and domain selection, seamlessly.</p>
+                <h1 className='text-3xl font-semibold text-black'>{headline}</h1>
+                <p className='font-medium text-gray-500'>{details}</p>
                 <button className='mt-5 bg-black px-4 py-2 text-white rounded-full font-medium transition-all flex items-center gap-2 hover:gap-3'>
                     Read More <span><MoveRight /></span>
                 </button>
